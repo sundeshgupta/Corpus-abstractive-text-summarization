8cf3eef3294c98147d7cf9c6ebe6f0da0e8e2ef2
jains8844 <jains8844@gmail.com>
2020-05-27 02:39:28 +0530

    Evaluation template addition

---
+++ app/static/script.js
@@ -0,0 +1,24 @@
+document.getElementById('form1').addEventListener('submit', function(event){
+	event.preventDefault();
+	var xhr = new XMLHttpRequest();
+	var url = '/api/summarize';
+	xhr.open('POST', url, 'true');
+	xhr.onload = function() {
+		if(xhr.readyState==4 && xhr.status == '201') {
+			console.log(xhr.responseText);
+			var res = JSON.parse(xhr.responseText);
+			document.getElementById('summary').innerHTML = res.summary;
+		}
+		else {
+			var res = '{"summary":"sample summary"}';
+			var res1 = JSON.parse(res);
+			document.getElementById('summary').innerHTML = res1.summary;
+		}
+	}
+	var data = {};
+	data.text = document.getElementById('textarea1').value;
+	var json = JSON.stringify(data);
+	console.log(json);
+	xhr.setRequestHeader('Content-Type', 'application/json');
+	xhr.send(json);
+});
